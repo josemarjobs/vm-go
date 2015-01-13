6626599afc1e240ccb88ceafb99f7769a083c847
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2015-01-13 16:17:17 -0200

    Executing Add

--- vm.go
+++ vm.go
@@ -1,4 +1,6 @@
 package main
+
+import "fmt"
 	
 const (
 	PUSH = iota
@@ -15,7 +17,41 @@ type VM struct {
 }
 
 func (v *VM) run(c []int) {
-	
+	v.stack = make([]int, 100)
+	v.sp = -1
+
+	v.code = c
+	v.pc = 0
+
+	for {
+		// Fetch
+		op := v.code[v.pc]
+		v.pc++
+
+		// Decode
+		switch op {
+		case PUSH:
+			val := v.code[v.pc]
+			v.pc++
+
+			v.sp++
+			v.stack[v.sp] = val
+		case ADD:
+			a := v.stack[v.sp]
+			v.sp--;
+			b := v.stack[v.sp]
+			v.sp--
+
+			v.sp++
+			v.stack[v.sp] = a+b
+		case PRINT:
+			val := v.stack[v.sp]
+			v.sp--
+			fmt.Println(val)
+		case HALT:
+			return;
+		}
+	}
 }
 func main() {
 	code := []int{
